463b730f67b291c539516f3af36e218f1f7b86ee
dzeju <62911257+dzeju@users.noreply.github.com>
2020-04-23 21:31:44 +0200

    Initial commit

---
+++ Biblioteka/AddItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Biblioteka
+{
+    class AddItem
+    {
+        public void FileOpen()
+        {
+            var fileDialog = new OpenFileDialog();
+            var result = fileDialog.ShowDialog();
+            switch(result)
+            {
+                case DialogResult.OK:
+                    string file = fileDialog.FileName;
+                    DataRead(file);
+                    break;
+                case DialogResult.Cancel:
+                default:
+                    break;
+            }
+        }
+
+        private void DataRead(string file)
+        {
+            TagLib.File f = TagLib.File.Create(file);
+            MessageBox.Show(f.Tag.Title);
+            
+        }
+
+    }
+}
